d2cc2e85ba19f54de95cc57031f8e0ce4c22dc27
aditisingh0720 <aditisingh0720@gmail.com>
2025-07-25 00:09:38 +0530

    app and home js changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import RegisterChitFund from './components/RegisterChitFund';
-import RegisterUser from './components/RegisterUser';
-import FundDetails from './FundDetails';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Component imports
+import Home from "./components/Home";
+import RegisterChitFund from "./components/RegisterChitFund";
+import RegisterUser from "./components/RegisterUser";
+import FundDetails from "./FundDetails";
+import PendingTransactions from "./components/PendingTransactions";
+import BlockDetails from "./components/BlockDetails";
+import UserTrans from "./components/UserTrans";
+import CurrentParticipants from "./components/CurrentParticipants";
 function App() {
   return (
     <Router>
@@ -13,6 +18,11 @@ function App() {
         <Route path="/register-chit-fund" element={<RegisterChitFund />} />
         <Route path="/register-user" element={<RegisterUser />} />
         <Route path="/fund/:id" element={<FundDetails />} />
+        <Route path="/pending-transactions" element={<PendingTransactions />} />
+        <Route path="/blocks/:blockId" element={<BlockDetails />} />
+        <Route path="/user" element={<UserTrans />} />
+        <Route path="/participants" element={<CurrentParticipants />} />{" "}
+        {/* ✅ Added this */}
       </Routes>
     </Router>
   );
